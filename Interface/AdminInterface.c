db14736a6639d111ad1b1cd820102a56c4b250d0
ntuanh <ntuanh2705@gmail.com>
2024-12-28 14:01:25 +0000

    [ADD] Admin Interface

---
+++ Interface/AdminInterface.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <time.h>
+
+// #include "../Objects/history.h"
+// #include "../Objects/hotels.h"
+// #include "../Objects/users.h"
+#include "AdminInterface.h"
+
+int adminInterface(historyManager manager, ListHotel hotels, ListUser users)
+{
+    printf("WELCOME TO ADMIN INTERFACE \n");
+    return 3 ;
+}
---
+++ Interface/AdminInterface.h
@@ -0,0 +1,10 @@
+#ifndef ADMININTERFACE_H
+#define ADMININTERFACE_H 
+#include "../Objects/history.h"
+#include "../Objects/hotels.h"
+#include "../Objects/users.h"
+
+int adminInterface(historyManager manager, ListHotel hotels, ListUser users);
+
+
+#endif 
